c89f919b259d32b8dcc93b46d6e72e37414e2665
Andrew <andreidmarket@mail.ru>
2021-05-13 23:33:59 +0500

    sqlite works!

--- src/main/java/com/ununtrium/hotel/HotelApplication.java
+++ src/main/java/com/ununtrium/hotel/HotelApplication.java
@@ -2,12 +2,24 @@ package com.ununtrium.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class HotelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+		dataSourceBuilder.url("jdbc:sqlite:main.db");
+		return dataSourceBuilder.build();
 	}
 
 }
